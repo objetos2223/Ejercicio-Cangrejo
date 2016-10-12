512f88f319f3a6e11a6bae517858c3edfc9c578a
Programación Orientada a Objetos <objetos2223@gmail.com>
2016-10-12 01:14:34 -0500

    Revert "otrometodo2"

--- modern-crab/Spider.java
+++ modern-crab/Spider.java
@@ -3,7 +3,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class Spider here.
  * 
- * @author Diego
+ * @author Eloy
  * @version (a version number or a date)
  */
 public class Spider extends Actor
@@ -17,10 +17,10 @@ public class Spider extends Actor
         // Add your action code here.
     }    
     
-    public void otrometodo()
+    public void metodo2()
     {
-        if (3 < 4)  {
-            System.out.println("otrometodo");
-        }
+        System.out.println("Hola mundo");
     }
+    
+    
 }
