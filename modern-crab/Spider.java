54eb598559614e2d50697745befaabf3ded342c1
Programación Orientada a Objetos <objetos2223@gmail.com>
2016-10-11 23:21:30 -0500

    Modifica Spider
    Modificación de la clase Spider

--- modern-crab/Spider.java
+++ modern-crab/Spider.java
@@ -3,7 +3,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class Spider here.
  * 
- * @author (your name) 
+ * @author Eloy
  * @version (a version number or a date)
  */
 public class Spider extends Actor
